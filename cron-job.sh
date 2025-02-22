f12eb7662ac51f35c0562750c8195d9ac1f7f32a
4shadoww <4shadoww0@gmail.com>
2025-02-22 16:14:14 +0200

    Update cronjob

--- cron-job.sh
+++ cron-job.sh
@@ -17,7 +17,12 @@ if [[ $? = 0 && `wc -l<access.csv` -ge 2  ]]; then
 fi
 
 # Parse auth log
-python parse_auth_logs.py /var/log/auth.log.1 -o auth.csv -d $today
+python parse_auth_logs.py /var/log/auth.log.1 -o auth1.csv -d $today
+python parse_auth_logs.py /var/log/auth.log -o auth.csv -d $today
+
+if [[ $? = 0 && `wc -l<auth1.csv` -ge 2 ]]; then
+    python send-bulk.py $1 auth1.csv
+fi
 
 if [[ $? = 0 && `wc -l<auth.csv` -ge 2 ]]; then
     python send-bulk.py $1 auth.csv
